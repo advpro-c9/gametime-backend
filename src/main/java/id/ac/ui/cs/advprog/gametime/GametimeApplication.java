45e07907584048d0a24fa18077c1061b1e7e2f79
SyauqiArman <syauqiarmanaya@gmail.com>
2024-04-04 22:14:39 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/gametime/GametimeApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gametime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametimeApplication.class, args);
+	}
+
+}
