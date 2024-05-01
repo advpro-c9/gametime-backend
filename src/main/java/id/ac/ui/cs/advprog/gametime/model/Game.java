81f53f36b5b5547c29e653192b6c162791a608f0
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-01 18:31:55 +0700

    [GREEN] Implement Game model

---
+++ src/main/java/id/ac/ui/cs/advprog/gametime/model/Game.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.gametime.model;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Game {
+    private String gameId;
+    private String gameName;
+    private String gameDescription;
+    private List<String> gameGenres;
+    private double gamePrice;
+}
