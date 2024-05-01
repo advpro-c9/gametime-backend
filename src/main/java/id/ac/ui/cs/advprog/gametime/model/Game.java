9bc9a0affe2b50a72184b4a0448940eae0c6fb4e
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-01 20:31:06 +0700

    start to use JPA

--- src/main/java/id/ac/ui/cs/advprog/gametime/model/Game.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/model/Game.java
@@ -1,13 +1,22 @@
 package id.ac.ui.cs.advprog.gametime.model;
 import java.util.List;
 
+import jakarta.persistence.*;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @Entity @NoArgsConstructor @Table(name = "Game")
 public class Game {
+    @Id @Column(name = "game_id")
     private String gameId;
+    @Column(name = "game_name")
     private String gameName;
+    @ElementCollection
+    @CollectionTable(name = "game_genres", joinColumns = @JoinColumn(name = "gameId"))
+    @Column(name = "game_genre")
     private List<String> gameGenres;
+    @Column(name = "game_price")
     private double gamePrice;
 }
