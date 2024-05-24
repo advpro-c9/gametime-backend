abc58668d7cb72d4efe3a87e61e162d98a7c90ce
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-24 18:59:42 +0700

    [GREEN] New service implement

--- src/main/java/id/ac/ui/cs/advprog/gametime/service/SearchFilterService.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/service/SearchFilterService.java
@@ -1,12 +1,8 @@
 package id.ac.ui.cs.advprog.gametime.service;
+
 import id.ac.ui.cs.advprog.gametime.model.Game;
 import java.util.List;
 
 public interface SearchFilterService {
-    Game create(Game game);
-    List<Game> findAll();
-    Game findById(String gameId);
-    List<Game> findByName(String gameName);
-    List<Game> findByPriceRange(double minPrice, double maxPrice);
-    List<Game> findByGenres(List<String> genres);
+    List<Game> search(String type, String input);
 }
--- src/main/java/id/ac/ui/cs/advprog/gametime/service/SearchFilterServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/service/SearchFilterServiceImpl.java
@@ -1,48 +1,28 @@
 package id.ac.ui.cs.advprog.gametime.service;
+
 import id.ac.ui.cs.advprog.gametime.model.Game;
 import id.ac.ui.cs.advprog.gametime.repository.SearchFilterRepository;
+import id.ac.ui.cs.advprog.gametime.strategy.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org. springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util. Iterator;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class SearchFilterServiceImpl implements SearchFilterService {
+
+    private final SearchFilterStrategyType searchFilterStrategyType;
+    private final SearchFilterRepository searchFilterRepository;
+
     @Autowired
-    private SearchFilterRepository searchFilterRepository;
-
-    @Override
-    public Game create(Game game) {
-        searchFilterRepository.create(game);
-        return game;
+    public SearchFilterServiceImpl(SearchFilterStrategyType searchFilterStrategyType, SearchFilterRepository searchFilterRepository) {
+        this.searchFilterStrategyType = searchFilterStrategyType;
+        this.searchFilterRepository = searchFilterRepository;
     }
-
-    @Override
-    public List<Game> findAll() {
-        Iterator<Game> gameIterator = searchFilterRepository.findAll();
-        List<Game> allGame = new ArrayList<>();
-        gameIterator.forEachRemaining(allGame::add);
-        return allGame;
-    }
-
-    @Override
-    public Game findById(String gameId){
-        return searchFilterRepository.findById(gameId);
-    }
-
-    @Override
-    public List<Game> findByName(String gameName){
-        return searchFilterRepository.findByName(gameName);
-    }
-
-    @Override
-    public List<Game> findByPriceRange(double minPrice, double maxPrice){
-        return searchFilterRepository.findByPriceRange(minPrice, maxPrice);
-    }
-
-    @Override
-    public List<Game> findByGenres (List<String> genres){
-        return searchFilterRepository.findByGenres(genres);
+    public List<Game> search(String type, String input) {
+        SearchFilterStrategy strategy = searchFilterStrategyType.getStrategy(type, searchFilterRepository);
+        if (strategy != null) {
+            return strategy.search(input);
+        }
+        throw new IllegalArgumentException("Tipe " + type + " tidak sesuai untuk dicari");
     }
 }
