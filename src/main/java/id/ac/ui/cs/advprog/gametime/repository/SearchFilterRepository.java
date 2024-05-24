c908c5dd2c8dff837cdb51339bf0c3d84ce65d28
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-24 16:53:02 +0700

    [REFACTOR] SearchFilterRepository with JpaRepository

--- src/main/java/id/ac/ui/cs/advprog/gametime/repository/SearchFilterRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/repository/SearchFilterRepository.java
@@ -1,75 +1,15 @@
 package id.ac.ui.cs.advprog.gametime.repository;
+
 import id.ac.ui.cs.advprog.gametime.model.Game;
 import org. springframework.stereotype.Repository;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public class SearchFilterRepository {
+public interface SearchFilterRepository extends JpaRepository<Game, UUID>{
 
-    private final List<Game> gameData = new ArrayList<>();
-
-    public Game create (Game game) {
-        if(game.getGameId() == null){
-            UUID uuid = UUID.randomUUID();
-            game.setGameId(uuid.toString());
-        }
-        gameData.add(game);
-        return game;
-    }
-
-    public Iterator<Game> findAll(){
-        return gameData.iterator();
-    }
-
-    public Game findById(String id) {
-        for (Game game : gameData) {
-            if (game.getGameId().equals(id)) {
-                return game;
-            }
-        }
-        return null;
-    }
-
-    public List<Game> findByName(String searchedGameName) {
-        List<Game> result = new ArrayList<>();
-        for (Game game : gameData) {
-            if (game.getGameName().equalsIgnoreCase(searchedGameName)) {
-                result.add(game);
-            }
-        }
-        return result;
-    }
-
-    public List<Game> findByPriceRange(double minPrice, double maxPrice) {
-        List<Game> gamesInRange = new ArrayList<>();
-        for (Game game : gameData) {
-            double gamePrice = game.getGamePrice();
-            if (gamePrice >= minPrice && gamePrice <= maxPrice) {
-                gamesInRange.add(game);
-            }
-        }
-        return gamesInRange;
-    }
-
-    public List<Game> findByGenres(List<String> searchedGenres) {
-        List<Game> matchingGames = new ArrayList<>();
-        for (Game game : gameData) {
-            if (containsAnyGenres(game.getGameGenres(), searchedGenres)) {
-                matchingGames.add(game);
-            }
-        }
-        return matchingGames;
-    }
-
-    private boolean containsAnyGenres(List<String> gameGenres, List<String> searchedGenres) {
-        for (String genre : gameGenres) {
-            if (searchedGenres.contains(genre)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    List<Game> findByGameNameContaining(String name);
+    List<Game> findByGameGenreContaining(String genre);
+    List<Game> findByGamePriceBetween(double minPrice, double maxPrice);
 }
--- src/test/java/id/ac/ui/cs/advprog/gametime/repository/SearchFilterRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/gametime/repository/SearchFilterRepositoryTest.java
@@ -3,192 +3,110 @@ package id.ac.ui.cs.advprog.gametime.repository;
 import id.ac.ui.cs.advprog.gametime.model.Game;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+//@DataJpaTest
+@ExtendWith(MockitoExtension.class)
 class SearchFilterRepositoryTest {
 
-    SearchFilterRepository searchFilterRepository;
-    List<Game> games;
+    @Mock
+    private SearchFilterRepository searchFilterRepository;
+
+    @InjectMocks
+    private Game game;
+    private final UUID id = UUID.randomUUID();
 
     @BeforeEach
-    void setUp() {
-        searchFilterRepository = new SearchFilterRepository();
-        games = new ArrayList<>();
-
-        Game game1 = new Game();
-        game1.setGameId("1");
-        game1.setGameName("Game A");
-        game1.setGameDescription("Desc Game A");
-        game1.setGamePrice(20.0);
-        game1.setGameGenres(Arrays.asList("Action", "Adventure"));
-        games.add(game1);
-
-        Game game2 = new Game();
-        game2.setGameId("2");
-        game2.setGameName("Game B");
-        game2.setGameDescription("Desc Game B");
-        game2.setGamePrice(30.0);
-        game2.setGameGenres(Arrays.asList("Strategy", "Action"));
-        games.add(game2);
-
-        Game game3 = new Game();
-        game3.setGameId("3");
-        game3.setGameName("Game C");
-        game3.setGameDescription("Desc Game B");
-        game3.setGamePrice(40.0);
-        game3.setGameGenres(Arrays.asList("RPG", "Adventure"));
-        games.add(game3);
+    public void setUp() {
+        game = new Game();
+        game.setGameId(id);
+        game.setGameName("Test Game");
+        game.setGameDescription("Test Game Description");
+        game.setGameGenre("Adventure");
+        game.setGamePrice(49.99);
     }
 
     @Test
-    void testCreate() {
-        Game game = games.get(1);
-        Game result = searchFilterRepository.create(game);
-
-        Game findResult = searchFilterRepository.findById(games.get(1).getGameId());
-        assertEquals(game.getGameId(), result.getGameId());
-        assertEquals(game.getGameId(), findResult.getGameId());
-        assertEquals(game.getGameName(), findResult.getGameName());
-        assertEquals(game.getGameDescription(), findResult.getGameDescription());
-        assertEquals(game.getGamePrice(), findResult.getGamePrice());
-        assertEquals(game.getGameGenres(), findResult.getGameGenres());
+    void testFindByGameNameContaining() {
+        when(searchFilterRepository.findByGameNameContaining(anyString())).thenReturn(Arrays.asList(game));
+        List<Game> foundGames = searchFilterRepository.findByGameNameContaining("Test");
+        verify(searchFilterRepository, times(1)).findByGameNameContaining("Test");
     }
 
     @Test
-    void testCreateInvalid() {
-        Game game0 = games.get(0);
-        Game game1 = games.get(1);
-        Game result = searchFilterRepository.create(game0);
-
-        assertNotEquals(game1, result);
+    void testFindByGameGenreIn() {
+        when(searchFilterRepository.findByGameGenreContaining(anyString())).thenReturn(Arrays.asList(game));
+        List<Game> foundGames = searchFilterRepository.findByGameGenreContaining("Adventure");
+        verify(searchFilterRepository, times(1)).findByGameGenreContaining("Adventure");
     }
 
     @Test
-    void testFindAll() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        Iterator<Game> iterator = searchFilterRepository.findAll();
-        int count = 0;
-        while (iterator.hasNext()) {
-            Game nextGame = iterator.next();
-            assertTrue(games.contains(nextGame));
-            count++;
-        }
-
-        assertEquals(games.size(), count);
+    void testFindByGamePriceBetween() {
+        when(searchFilterRepository.findByGamePriceBetween(anyDouble(), anyDouble())).thenReturn(Arrays.asList(game));
+        List<Game> foundGames = searchFilterRepository.findByGamePriceBetween(30.00, 60.00);
+        verify(searchFilterRepository, times(1)).findByGamePriceBetween(30.00, 60.00);
     }
 
     @Test
-    void testFindAllInvalid() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        Iterator<Game> iterator = searchFilterRepository.findAll();
-        int count = 0;
-        while (iterator.hasNext()) {
-            Game nextGame = iterator.next();
-            assertTrue(games.contains(nextGame));
-            count++;
-        }
-
-        assertNotEquals(games.size()+1, count);
+    void testFindByGameNameContaining_NoResult() {
+        when(searchFilterRepository.findByGameNameContaining(anyString())).thenReturn(Collections.emptyList());
+        List<Game> foundGames = searchFilterRepository.findByGameNameContaining("NonExistingGame");
+        assertTrue(foundGames.isEmpty(), "The list of found games should be empty");
+        verify(searchFilterRepository, times(1)).findByGameNameContaining("NonExistingGame");
     }
 
     @Test
-    void testFindById() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        Game findResult = searchFilterRepository.findById(games.get(1).getGameId());
-        assertEquals(games.get(1).getGameId(), findResult.getGameId());
-        assertEquals(games.get(1).getGameName(), findResult.getGameName());
-        assertEquals(games.get(1).getGameDescription(), findResult.getGameDescription());
-        assertEquals(games.get(1).getGamePrice(), findResult.getGamePrice());
-        assertEquals(games.get(1).getGameGenres(), findResult.getGameGenres());
+    void testFindByGameGenreContaining_NoResult() {
+        when(searchFilterRepository.findByGameGenreContaining(anyString())).thenReturn(Collections.emptyList());
+        List<Game> foundGames = searchFilterRepository.findByGameGenreContaining("NonExistingGenre");
+        assertTrue(foundGames.isEmpty(), "The list of found games should be empty");
+        verify(searchFilterRepository, times(1)).findByGameGenreContaining("NonExistingGenre");
     }
 
     @Test
-    void testFindByIdInvalid() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        assertNull(searchFilterRepository.findById("999"));
+    void testFindByGamePriceBetween_NoResult() {
+        when(searchFilterRepository.findByGamePriceBetween(anyDouble(), anyDouble())).thenReturn(Collections.emptyList());
+        List<Game> foundGames = searchFilterRepository.findByGamePriceBetween(100.00, 200.00);
+        assertTrue(foundGames.isEmpty(), "The list of found games should be empty");
+        verify(searchFilterRepository, times(1)).findByGamePriceBetween(100.00, 200.00);
     }
 
     @Test
-    void testFindByName() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        List <Game> findResult = searchFilterRepository.findByName(games.get(1).getGameName());
-        assertEquals(games.get(1).getGameId(), findResult.getFirst().getGameId());
-        assertEquals(games.get(1).getGameName(), findResult.getFirst().getGameName());
-        assertEquals(games.get(1).getGameDescription(), findResult.getFirst().getGameDescription());
-        assertEquals(games.get(1).getGamePrice(), findResult.getFirst().getGamePrice());
-        assertEquals(games.get(1).getGameGenres(), findResult.getFirst().getGameGenres());
+    void testFindByGameNameContaining_Exception() {
+        when(searchFilterRepository.findByGameNameContaining(anyString())).thenThrow(new RuntimeException("Database error"));
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            searchFilterRepository.findByGameNameContaining("Test");
+        });
+        assertEquals("Database error", exception.getMessage());
+        verify(searchFilterRepository, times(1)).findByGameNameContaining("Test");
     }
 
     @Test
-    void testFindByNameInvalid() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        assertTrue(searchFilterRepository.findByName("Nonexistent Game").isEmpty());
+    void testFindByGameGenreContaining_Exception() {
+        when(searchFilterRepository.findByGameGenreContaining(anyString())).thenThrow(new RuntimeException("Database error"));
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            searchFilterRepository.findByGameGenreContaining("Adventure");
+        });
+        assertEquals("Database error", exception.getMessage());
+        verify(searchFilterRepository, times(1)).findByGameGenreContaining("Adventure");
     }
 
     @Test
-    void testFindByPriceRange() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        List<Game> findResult = searchFilterRepository.findByPriceRange(20.0, 35.0);
-        assertEquals(games.get(1).getGameId(), findResult.get(1).getGameId());
-        assertEquals(games.get(1).getGameName(), findResult.get(1).getGameName());
-        assertEquals(games.get(1).getGameDescription(), findResult.get(1).getGameDescription());
-        assertEquals(games.get(1).getGamePrice(), findResult.get(1).getGamePrice());
-        assertEquals(games.get(1).getGameGenres(), findResult.get(1).getGameGenres());
+    void testFindByGamePriceBetween_Exception() {
+        when(searchFilterRepository.findByGamePriceBetween(anyDouble(), anyDouble())).thenThrow(new RuntimeException("Database error"));
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            searchFilterRepository.findByGamePriceBetween(30.00, 60.00);
+        });
+        assertEquals("Database error", exception.getMessage());
+        verify(searchFilterRepository, times(1)).findByGamePriceBetween(30.00, 60.00);
     }
 
-    @Test
-    void testFindByPriceRangeInvalid() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        assertTrue(searchFilterRepository.findByPriceRange(10.0, 15.0).isEmpty());
-    }
-
-    @Test
-    void testFindByGenres() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        List<Game> findResult = searchFilterRepository.findByGenres(Arrays.asList("Strategy", "Action"));
-        assertEquals(games.get(1).getGameId(), findResult.get(1).getGameId());
-        assertEquals(games.get(1).getGameName(), findResult.get(1).getGameName());
-        assertEquals(games.get(1).getGameDescription(), findResult.get(1).getGameDescription());
-        assertEquals(games.get(1).getGamePrice(), findResult.get(1).getGamePrice());
-        assertEquals(games.get(1).getGameGenres(), findResult.get(1).getGameGenres());
-    }
-
-    @Test
-    void testFindByGenresInvalid() {
-        for (Game game : games) {
-            searchFilterRepository.create(game);
-        }
-
-        assertTrue(searchFilterRepository.findByGenres(Arrays.asList("Puzzle", "Simulation")).isEmpty());
-    }
 }
