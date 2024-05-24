b8389883fb2492432a92104764cc14888c24ac11
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-24 19:07:56 +0700

    [GREEN] Change search controller with REST API

--- src/main/java/id/ac/ui/cs/advprog/gametime/controller/SearchFilterController.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/controller/SearchFilterController.java
@@ -15,43 +15,8 @@ public class SearchFilterController {
     @Autowired
     private SearchFilterService searchFilterService;
 
-    @PostMapping
-    public ResponseEntity<Game> createGame(@RequestBody Game game) {
-        Game createdGame = searchFilterService.create(game);
-        return ResponseEntity.ok(createdGame);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Game>> getAllGames() {
-        List<Game> games = searchFilterService.findAll();
-        return ResponseEntity.ok(games);
-    }
-
-    @GetMapping("/{gameId}")
-    public ResponseEntity<Game> getGameById(@PathVariable String gameId) {
-        Game game = searchFilterService.findById(gameId);
-        if (game != null) {
-            return ResponseEntity.ok(game);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @GetMapping("/name/{gameName}")
-    public ResponseEntity<List<Game>> getGamesByName(@PathVariable String gameName) {
-        List<Game> games = searchFilterService.findByName(gameName);
-        return ResponseEntity.ok(games);
-    }
-
-    @GetMapping("/price")
-    public ResponseEntity<List<Game>> getGamesByPriceRange(@RequestParam double minPrice, @RequestParam double maxPrice) {
-        List<Game> games = searchFilterService.findByPriceRange(minPrice, maxPrice);
-        return ResponseEntity.ok(games);
-    }
-
-    @GetMapping("/genres")
-    public ResponseEntity<List<Game>> getGamesByGenres(@RequestParam List<String> genres) {
-        List<Game> games = searchFilterService.findByGenres(genres);
-        return ResponseEntity.ok(games);
+    @GetMapping("/search/{type}/{input}")
+    public ResponseEntity<List<Game>> searchGame(@PathVariable String type, @PathVariable String input) {
+        return ResponseEntity.ok(searchFilterService.search(type, input));
     }
 }
