00118ef89f2facb5319aea91e171ac6ce3a0a0f2
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-24 18:42:47 +0700

    [GREEN] Implement Price Strategy

---
+++ src/main/java/id/ac/ui/cs/advprog/gametime/strategy/PriceRangeSearchStrategy.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.gametime.strategy;
+
+import id.ac.ui.cs.advprog.gametime.model.Game;
+import id.ac.ui.cs.advprog.gametime.repository.SearchFilterRepository;
+import java.util.List;
+
+public class PriceRangeSearchStrategy implements SearchFilterStrategy {
+
+    private SearchFilterRepository searchFilterRepository;
+
+    public PriceRangeSearchStrategy(SearchFilterRepository searchFilterRepository) {
+        this.searchFilterRepository = searchFilterRepository;
+    }
+
+    @Override
+    public List<Game> search(String gamePriceRange) {
+        String[] prices = gamePriceRange.split(",");
+        double minPrice = Double.parseDouble(prices[0]);
+        double maxPrice = Double.parseDouble(prices[1]);
+        return searchFilterRepository.findByGamePriceBetween(minPrice, maxPrice);
+    }
+}
